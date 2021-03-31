ce4806260854b9aa96b99186226bb07927f97aa8
joako81 <joaquin.rosa.82@gmail.com>
2021-03-31 21:37:12 +0200

    creating logo and stiles

---
+++ src/components/logo/logo.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+              <img src="../assets/ds_circle_logo.png"></img>
+
+                
+            </div>
+        );
+    }
+}
+
+export default Logo;
