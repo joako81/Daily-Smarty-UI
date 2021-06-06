26c212511e554a16aaa6983988f2d9ff40a0d01e
Joaquín Rosa <joaquin.rosa.82@gmail.com>
2021-06-06 13:19:34 +0200

    setup redux devtools and fixed reducer for set_recent_posts

--- src/reducers/postsReducer1.js
+++ src/reducers/postsReducer1.js
@@ -11,10 +11,11 @@ export default function(state = INIT_STATE, action) {
     switch (action.type) {
         case SET_RECENT_POSTS:
             const recentPosts = action.payload;
-
-            return [...state, recentPosts]
+            return {
+                    ...state,
+                    recentPosts
+                }
         default:
             return state;
     }
 } 
-
