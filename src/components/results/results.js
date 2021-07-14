c3de59fd97b683f2d5c36b2485fb10d69a3f8a15
Joaquín Rosa <joaquin.rosa.82@gmail.com>
2021-07-14 22:37:22 +0200

    put together fetchPostsWithQuery action to receive queried posts

--- src/components/results/results.js
+++ src/components/results/results.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import Logo from '../logo/logo';
 import SearchBar from '../searchBar/searchBar';
 
+import {connect} from 'react-redux';
+import * as actions from '../../actions';
+
 class Results extends Component {
 
     handleSearchBarSubmit (query) {
-        console.log(query);
+        this.props.fetchPostsWithQuery(query);
 
     }
 
@@ -20,4 +23,4 @@ class Results extends Component {
     }
 }
 
-export default Results;
+export default connect(null, actions)(Results);
