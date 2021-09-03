0e6b161c5d28c235f83beb2336111d6c40dda579
joaquin <joaquin.rosa.82@gmail.com>
2021-09-03 21:24:18 +0200

    check code

--- src/components/recentPosts/postsReducer.js
+++ src/components/recentPosts/postsReducer.js
@@ -1,9 +1,8 @@
-import {
-    SET_RECENT_POSTS
-} from '../actions/types';
+import { SET_RESULTS_POSTS } from '../../actions/types';
+import { SET_RECENT_POSTS } from '../../actions/types';
 
 const INIT_STATE = {
-    posts: [],
+    resultsPosts: [],
     recentPosts: []
 }
 
@@ -13,6 +12,12 @@ export default function(state = INIT_STATE, action) {
             const recentPosts = action.payload;
 
             return [...state, recentPosts]
+
+        case SET_RESULTS_POSTS:
+            const resultsPosts= action.payload;
+
+            return [...state, resultsPosts]
+        
         default:
             return state;
     }
--- src/components/results/results.js
+++ src/components/results/results.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Logo from '../logo/logo';
 import SearchBar from '../searchBar/searchBar';
+import ResultsPosts from '../resultsPosts/resultsPosts';
 
 import {connect} from 'react-redux';
 import * as actions from '../../actions';
 
 class Results extends Component {
 
-    handleSearchBarSubmit (query) {
+    handleSearchBarSubmit ({query}) {
         this.props.fetchPostsWithQuery(query);
 
     }
@@ -17,7 +18,7 @@ class Results extends Component {
             <div>
                <Logo size={55}/>
                <SearchBar onsubmit = {(query)=> this.handleSearchBarSubmit(query)}/>
-               
+               <ResultsPosts/>
             </div>
         );
     }
